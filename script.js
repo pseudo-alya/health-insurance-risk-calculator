1c171e297db56716326777d5e833ae0837b33fd5
Alya AlDiqqi <alya.ad233@gmail.com>
2025-03-16 15:45:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('riskForm').addEventListener('submit', function(event) {
     familyDisease: familyDisease
   };
 
-  fetch('https://health-insurance-calculator-exfhhhggf0hgb8fu.uaenorth-01.azurewebsites.net/calculate-risk', {
+  fetch('https://health-insurance-calculator-exfhhhggf0hgb8fu.uaenorth-01.azurewebsites.net', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
